9ab713e3466d6eb5eb428f387eb00aadf0e2de7b
noyishai <noyishai1@gmail.com>
2018-12-17 21:49:41 +0200

    Authentication

---
+++ Instagram/SignUpViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignUpViewController.swift
+//  Instagram
+//
+//  Created by admin on 08/12/2018.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismiss_onClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
