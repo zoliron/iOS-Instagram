75afdb06dcd7999074f7c5e32f28ba172874aab2
Ronen <ronen@Ronens-Mac.local>
2018-12-20 11:18:52 -0800

    Navigation controller implementation Remove image titles Fix image offset

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Change the colors of the tab bar's items
+        UITabBar.appearance().tintColor = .black
         return true
     }
 
