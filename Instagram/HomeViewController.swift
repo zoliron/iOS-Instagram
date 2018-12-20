312f78ae689a141b817ccf0f7ad673af52769c6e
Ronen <ronen@Ronens-Mac.local>
2018-12-20 11:24:40 -0800

    New HomeViewController fix

--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -1,1 +1,18 @@
+//
+//  HomeiewController.swift
+//  Instagram
+//
+//  Created by admin on 18/12/2018.
+//  Copyright © 2018 admin. All rights reserved.
+//
 
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+}
