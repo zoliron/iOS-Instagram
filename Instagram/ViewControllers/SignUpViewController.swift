09ee62e8363decf165b42ab4533d6344048a7a9a
Zoliron <ronen1245@gmail.com>
2018-12-20 14:00:13 -0800

    Sign-In and Sign-Up new design + new folders for storyboards and view controllers

---
+++ Instagram/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SignUpViewController.swift
+//  Instagram
+//
+//  Created by admin on 08/12/2018.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        usernameTextField.backgroundColor = UIColor.clear
+        usernameTextField.tintColor = UIColor.white
+        usernameTextField.textColor = UIColor.white
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: usernameTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerUsername = CALayer()
+        bottomLayerUsername.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerUsername.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        usernameTextField.layer.addSublayer(bottomLayerUsername)
+        
+        
+        emailTextField.backgroundColor = UIColor.clear
+        emailTextField.tintColor = UIColor.white
+        emailTextField.textColor = UIColor.white
+        emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerEmail = CALayer()
+        bottomLayerEmail.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerEmail.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        emailTextField.layer.addSublayer(bottomLayerEmail)
+        
+        passwordTextField.backgroundColor = UIColor.clear
+        passwordTextField.tintColor = UIColor.white
+        passwordTextField.textColor = UIColor.white
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerPassword = CALayer()
+        bottomLayerPassword.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerPassword.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        passwordTextField.layer.addSublayer(bottomLayerPassword)
+        
+        profileImage.layer.cornerRadius = 40
+        profileImage.clipsToBounds = true
+    }
+    
+    @IBAction func dismiss_onClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
