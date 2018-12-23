68b5b0f42fc636217d34f473e70036521ec41387
Ronen Zolicha <ronen@Ronens-Mac.local>
2018-12-23 00:54:22 -0800

    Firebase integration

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -7,17 +7,19 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // Change the colors of the tab bar's items
         UITabBar.appearance().tintColor = .black
+        FirebaseApp.configure()
         return true
     }
 
