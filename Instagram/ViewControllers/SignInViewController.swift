693b204ecd5544f8e3f3420687f80156ad689f6c
zoliron <ronen1245@gmail.com>
2019-01-01 05:17:24 -0800

    Sign In button error checking implementation

--- Instagram/ViewControllers/SignInViewController.swift
+++ Instagram/ViewControllers/SignInViewController.swift
@@ -12,9 +12,12 @@ class SignInViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // emailTextField design
         emailTextField.backgroundColor = UIColor.clear
         emailTextField.tintColor = UIColor.white
         emailTextField.textColor = UIColor.white
@@ -24,6 +27,7 @@ class SignInViewController: UIViewController {
         bottomLayerEmail.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
         emailTextField.layer.addSublayer(bottomLayerEmail)
         
+        // passwordTextField design
         passwordTextField.backgroundColor = UIColor.clear
         passwordTextField.tintColor = UIColor.white
         passwordTextField.textColor = UIColor.white
@@ -32,5 +36,26 @@ class SignInViewController: UIViewController {
         bottomLayerPassword.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
         bottomLayerPassword.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
         passwordTextField.layer.addSublayer(bottomLayerPassword)
+        
+        // Observer to see if user input did change
+        handleTextField()
+    }
+    
+    // Observer to see if user input did change
+    func handleTextField() {
+        emailTextField.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
+        passwordTextField.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
+        
+    }
+    
+    // Checking if the userinputs are not empty and if not changing the Sign In botton color and enables is
+    func textFieldDidChange() {
+        guard let email = emailTextField.text, !email.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
+            signInButton.setTitleColor(UIColor.lightText, for: UIControlState.normal)
+            signInButton.isEnabled = false
+            return
+        }
+        signInButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        signInButton.isEnabled = true
     }
 }
