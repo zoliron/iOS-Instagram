10e184ff04b6620935bb199e1e623fc9d80b824a
Noy Ishai <noyishai1@gmail.com>
2019-01-17 07:16:17 -0800

    Update the edit_profile feed with the user details.

--- Instagram/ViewControllers/SettingTableViewController.swift
+++ Instagram/ViewControllers/SettingTableViewController.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class SettingTableViewController: UITableViewController {
 
+    @IBOutlet weak var usernameTextFiled: UITextField!
+    @IBOutlet weak var emailTextFiled: UITextField!
+    @IBOutlet weak var profileImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = "Edit Profile"
+        fetchCurrentUser()
     }
 
+    func fetchCurrentUser(){
+        Api.User.observeCurrentUser {(user)in
+            self.usernameTextFiled.text = user.username
+            self.emailTextFiled.text = user.email
+            if let profileUrl = URL(string: user.profileImageUrl!){
+                self.profileImageView.sd_setImage(with: profileUrl)
+            }
+        }
+    }
+    
 }
