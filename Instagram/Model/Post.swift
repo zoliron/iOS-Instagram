c93ca1c74eabfcfd2059c95286f3c7adb7973d8c
Zoliron <ronen1245@gmail.com>
2019-02-09 07:11:15 -0800

    Post Sqlite implementation

--- Instagram/Model/Post.swift
+++ Instagram/Model/Post.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import FirebaseAuth
+import SQLite
 
 class Post {
     var caption: String?
@@ -19,6 +20,17 @@ class Post {
     var isLiked: Bool?
     var ratio:CGFloat?
     var timestamp: Int?
+    var lastUpdated: Double?
+    
+    init() {
+    }
+    
+    init(newCaption: String, newPhotoUrl: String, newUserId: String, newId: String) {
+        caption = newCaption
+        photoUrl = newPhotoUrl
+        uid = newUserId
+        id = newId
+    }
 }
 
 // Extension to Post which will replace inits for more clear coding instead of overriding inits
@@ -41,7 +53,74 @@ extension Post {
         }
         return post
     }
+}
+
+extension Post {
+    static func createTable(database: OpaquePointer?)  {
+        var errormsg: UnsafeMutablePointer<Int8>? = nil
+        let res = sqlite3_exec(database, "CREATE TABLE IF NOT EXISTS POSTS (ID TEXT PRIMARY KEY, CAPTION TEXT, PHOTOURL TEXT, UID TEXT)", nil, nil, &errormsg);
+        if(res != 0){
+            print("error creating table");
+            return
+        }
+    }
     
-    static func transformPostVideo() {
+    static func drop(database: OpaquePointer?)  {
+        var errormsg: UnsafeMutablePointer<Int8>? = nil
+        let res = sqlite3_exec(database, "DROP TABLE POSTS;", nil, nil, &errormsg);
+        if(res != 0){
+            print("error creating table");
+            return
+        }
+    }
+    
+    static func getAll(database: OpaquePointer?)->[Post]{
+        var sqlite3_stmt: OpaquePointer? = nil
+        var data = [Post]()
+        if (sqlite3_prepare_v2(database,"SELECT * from POSTS;",-1,&sqlite3_stmt,nil)
+            == SQLITE_OK){
+            while(sqlite3_step(sqlite3_stmt) == SQLITE_ROW){
+                let id = String(cString:sqlite3_column_text(sqlite3_stmt,0)!)
+                let caption = String(cString:sqlite3_column_text(sqlite3_stmt,1)!)
+                let photoUrl = String(cString:sqlite3_column_text(sqlite3_stmt,3)!)
+                let userId = String(cString:sqlite3_column_text(sqlite3_stmt,5)!)
+                data.append(Post(newCaption: caption, newPhotoUrl: photoUrl, newUserId: userId, newId: id))
+            }
+        }
+        sqlite3_finalize(sqlite3_stmt)
+        return data
+    }
+    
+    static func addNew(database: OpaquePointer?, post: Post){
+        var sqlite3_stmt: OpaquePointer? = nil
+        if (sqlite3_prepare_v2(database,"INSERT OR REPLACE INTO FEED(UID, POST) VALUES (?,?);",-1, &sqlite3_stmt,nil) == SQLITE_OK){
+            let id = post.id!.cString(using: .utf8)
+            let caption = post.caption!.cString(using: .utf8)
+            let photoUrl = post.photoUrl!.cString(using: .utf8)
+            let userId = post.uid!.cString(using: .utf8)
+
+            sqlite3_bind_text(sqlite3_stmt, 1, id,-1,nil);
+            sqlite3_bind_text(sqlite3_stmt, 2, caption,-1,nil);
+            sqlite3_bind_text(sqlite3_stmt, 4, photoUrl,-1,nil);
+            sqlite3_bind_text(sqlite3_stmt, 6, userId,-1,nil);
+
+            if(sqlite3_step(sqlite3_stmt) == SQLITE_DONE){
+                print("new row added succefully")
+            }
+        }
+        sqlite3_finalize(sqlite3_stmt)
+    }
+    
+    static func get(database: OpaquePointer?, byId:String)->Post?{
+        return nil;
+    }
+    
+    static func getLastUpdateDate(database: OpaquePointer?)->Double{
+        return LastUpdateDates.get(database: database, tableName: "posts")
+    }
+    
+    static func setLastUpdateDate(database: OpaquePointer?, date:Double){
+        LastUpdateDates.set(database: database, tableName: "posts", date: date);
     }
 }
+
