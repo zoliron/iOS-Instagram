f08df50491d7fa2f1b6dbdcbb9cbf7d1dbe92a61
Zoliron <ronen1245@gmail.com>
2019-03-13 02:47:50 -0700

    Feed taking data from firebase (can do it from SQL, its in comment)

--- Instagram/FeedApi.swift
+++ Instagram/FeedApi.swift
@@ -56,25 +56,25 @@ class FeedApi{
 //                    }
 //                    Post.setLastUpdateDate(database: self.modelSql.database, date: postsLastUpdated)
 //                    let postsFullData = Post.getAll(database: self.modelSql.database)
-//
+
                     Api.User.observeUser(withId: post.uid!, completion: { (user) in
                         
                         // SQL add new user
-                        UserModel.addNew(database: self.modelSql.database, user: user)
-                        if (user.lastUpdated != nil && user.lastUpdated! > usersLastUpdated) {
-                            usersLastUpdated = user.lastUpdated!
-                        }
-
-                        UserModel.setLastUpdateDate(database: self.modelSql.database, date: usersLastUpdated)
-                        let usersFullData = UserModel.getAll(database: self.modelSql.database)
-                        for user in usersFullData {
-//                            for post in postsFullData {
-                                    results.append((post, user))
-//                            }
-                        }
-//                        if post.uid == user.id {
-//                            results.append((post, user))
+//                        UserModel.addNew(database: self.modelSql.database, user: user)
+//                        if (user.lastUpdated != nil && user.lastUpdated! > usersLastUpdated) {
+//                            usersLastUpdated = user.lastUpdated!
 //                        }
+//
+//                        UserModel.setLastUpdateDate(database: self.modelSql.database, date: usersLastUpdated)
+//                        let usersFullData = UserModel.getAll(database: self.modelSql.database)
+//                        for user in usersFullData {
+//                            for post in postsFullData {
+//                                    results.append((post, user))
+//                            }
+//                        }
+                        if post.uid == user.id {
+                            results.append((post, user))
+                        }
                         //  results.insert((post, user), at: index)
                         myGroup.leave()
                     })
